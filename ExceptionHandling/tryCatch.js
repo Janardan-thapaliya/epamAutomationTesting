7cafd6fcbd6a51780cc19f689ef794974db3e00e
Janardan <janardan.thapaliya.dev@gmail.com>
2023-06-30 11:42:23 +0300

    Exception handling file add

---
+++ ExceptionHandling/tryCatch.js
@@ -0,0 +1,16 @@
+
+
+try{
+    console.log(a);
+    let a = 3;
+    throw new ReferenceError("let must be declared before use");
+}
+catch(error){
+    console.log("Error: " + error.message);
+}
+
+try {
+    let 
+} catch (error) {
+    
+}
